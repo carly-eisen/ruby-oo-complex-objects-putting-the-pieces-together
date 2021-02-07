c4fc066e36729d41551afd602435076fb8fdd08a
carly-eisen <carlyagerber@gmail.com>
2021-02-07 09:45:32 -0600

    commit tree 4bb8aa7f0e9e49ae421028a755d624ca89bfdd1f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("Some Title")
+book.turn_page
+
+# Whenever the tutorial says to 'run the specs', you should type the following
+# into your command line:
+# learn spec/01_book_spec.rb
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+# To run the specs, type `learn spec/02_shoe_spec.rb` on your command line.
+# option z to wrap text
+
+class Shoe
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
